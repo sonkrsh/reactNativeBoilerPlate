5efa2068f7070761da01b2101a904859b4ecfc75
sourav <sourav.singh@emoha.com>
2021-12-12 11:10:42 +0530

    react react-redux saga addded

--- App.js
+++ App.js
@@ -7,35 +7,14 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
+import {SafeAreaView, Text} from 'react-native';
 
 const App = () => {
- 
-
   return (
-    <SafeAreaView >
-      
+    <SafeAreaView>
+      <Text>souravDone</Text>
     </SafeAreaView>
   );
 };
-
 
 export default App;
